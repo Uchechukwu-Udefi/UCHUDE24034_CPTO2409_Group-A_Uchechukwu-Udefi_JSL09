0d3a02ddc0aecd3b7be1665e07eb2a8afa0f6d08
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-03-14 10:07:29 +0200

    Update background image

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+// Get a random image from Unsplash and set it as the background image of the body
+// The image is fetched using the Unsplash API
+
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.body.style.backgroundImage = `url(${data.urls.full})`;
+    })
+    
 
 
 
